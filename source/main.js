7d62089027ddc0927a2a18fd06602c6520cec795
Galen Cochrane <cochgale@isu.edu>
2017-10-18 02:49:16 -0600

    making fullscreen

--- source/main.js
+++ source/main.js
@@ -3,7 +3,18 @@
 // Initialization
 //-----------------------------------------------------------*/
 window.onload = function init() {
+
+  var desiredHeight = document.body.clientHeight;
+  var desiredWidth = document.body.clientWidth;
+  $("#gl-canvas").width(desiredWidth);
+  $("#gl-canvas").height(desiredHeight);
+  $("#instrBox").width(desiredWidth);
+
   var canvas = document.getElementById('gl-canvas');
+  // These next two lines give canvas correct resolution
+  canvas.width = $("#gl-canvas").width();
+  canvas.height = $("#gl-canvas").height();
+  
   physics.init();
   graphics.init(canvas);
   controls.setUpEvents(canvas);
